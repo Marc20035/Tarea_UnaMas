c57d3beb31f80b352fc05f1fca3408823c266a1f
tHeCreator <marcsabalete@gmail.com>
2023-03-21 18:55:12 +0100

    CommitMCD

---
+++ src/main/java/org/example/Ejercicio5/MaximoComunDivisor.java
@@ -0,0 +1,36 @@
+package org.example.Ejercicio5;
+
+import java.util.function.BiFunction;
+
+public class MaximoComunDivisor {
+    public int mcdRecursivo(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return mcdRecursivo(b, a % b);
+    }
+    public int mcdIterativo(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    public void mcdLambda(){
+        int a = 56;
+        int b = 98;
+
+        BiFunction<Integer, Integer, Integer> mcdLambda = (x, y) -> {
+            while (y != 0) {
+                int temp = y;
+                y = x % y;
+                x = temp;
+            }
+            return x;
+        };
+
+        System.out.println(mcdLambda.apply(a, b));
+    }
+}
+
